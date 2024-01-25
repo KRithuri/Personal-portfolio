031bea9002613bccffd1aef94ea0c6d79a0fa336
Karabo Rithuri <rithurikarabo@gmail.com>
2024-01-25 18:20:51 +0200

    initial commit of style switchher js file

---
+++ Portfolio-1/style-switcher.js
@@ -0,0 +1,7 @@
+// Toggle style switcher
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+
+
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
