b8e5987dc0e1c8dc46160a647f65708b504c9ea6
Karabo Rithuri <rithurikarabo@gmail.com>
2024-01-25 18:38:01 +0200

    function to chane theme colors

--- Portfolio-1/style-switcher.js
+++ Portfolio-1/style-switcher.js
@@ -14,3 +14,14 @@ window.addEventListener("scroll", () => {
 })
 
 /*Theme Colors */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.setAttribute("disabled",true);
+        }
+    })
+}
