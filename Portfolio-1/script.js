e7f64d3f06c6f42dfffa9dad6924d908735113e2
Karabo Rithuri <rithurikarabo@gmail.com>
2024-01-25 21:57:53 +0200

    change to typing animation

--- Portfolio-1/script.js
+++ Portfolio-1/script.js
@@ -1,6 +1,6 @@
 /*Typing animation */
 var typed = new Typed(".typing",{
-    strings: ["","Information Systems Undergraduate","Web Designer","Back-end Developer"],
+    strings: ["","Information Systems Undergraduate","Web Designer","Back-end Developer","Full Stack Developer "],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
