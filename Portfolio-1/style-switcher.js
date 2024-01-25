0d1299fe6bd460d10dd1781b71a4918a328453bc
Karabo Rithuri <rithurikarabo@gmail.com>
2024-01-25 19:09:03 +0200

    dark mode and lught mode theme

--- Portfolio-1/style-switcher.js
+++ Portfolio-1/style-switcher.js
@@ -25,3 +25,19 @@ function setActiveStyle(color){
         }
     })
 }
+
+/*Theme Light and dark mode */
+const daynight = document.querySelector(".day-night");
+daynight.addEventListener("click", () => {
+    daynight.querySelector("i").classList.toggle("fa-sun");
+    daynight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark")) {
+        daynight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        daynight.querySelector("i").classList.add("fa-moon");
+    }
+})
