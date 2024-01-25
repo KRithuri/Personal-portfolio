92aa80441a298c4c89a9cb35229bd3408cc31bdf
Karabo Rithuri <rithurikarabo@gmail.com>
2024-01-25 21:08:13 +0200

    typing animation

---
+++ Portfolio-1/script.js
@@ -0,0 +1,7 @@
+/*Typing animation */
+var typed = new Typed(".typing",{
+    strings: ["","Information Systems Undergraduate","Web Designer","Back-end Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
